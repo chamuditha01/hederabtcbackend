6e3a187d92e1d70d5346559dafbad4dc80fca553
chamuditha01 <heshanchamuditha05@gmail.com>
2025-05-28 20:16:02 +0530

    update-280525-04

--- server.js
+++ server.js
@@ -1,49 +1,85 @@
+const express = require('express');
 const Web3 = require('web3');
+const cors = require('cors');
 
-const PRIVATE_KEY = '70a2a3ccdda50314d92e0ebf6596ef95943d1fc352c9c470fdbc688acddb4316';
+
+
+const PRIVATE_KEY = '70a2a3ccdda50314d92e0ebf6596ef95943d1fc352c9c470fdbc688acddb4316'; // Replace this with your backend account key
 const CONTRACT_ADDRESS = '0x0003f045459580fff5C9c987E747d73C86502bbb';
 const CONTRACT_ABI = [
-  {
-    "inputs": [],
-    "name": "owner",
-    "outputs": [{ "internalType": "address", "name": "", "type": "address" }],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      { "internalType": "address", "name": "player", "type": "address" },
-      { "internalType": "int256", "name": "prediction", "type": "int256" },
-      { "internalType": "uint256", "name": "amount", "type": "uint256" }
-    ],
-    "name": "placeBet",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [{ "internalType": "int256", "name": "actualPrice", "type": "int256" }],
-    "name": "resolveRound",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }
+	
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "player",
+				"type": "address"
+			},
+			{
+				"internalType": "int256",
+				"name": "prediction",
+				"type": "int256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "placeBet",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "int256",
+				"name": "actualPrice",
+				"type": "int256"
+			}
+		],
+		"name": "resolveRound",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	
 ];
-
 const RPC_URL = 'https://testnet.hashio.io/api';
 
+const app = express();
+app.use(express.json());
+
+app.use(cors({
+  origin: 'https://btcdapp.netlify.app',
+}));
+
+// Set up web3 and contract
 const web3 = new Web3(new Web3.providers.HttpProvider(RPC_URL));
 const backendAccount = web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY);
 web3.eth.accounts.wallet.add(backendAccount);
 
 const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
-
 let nonceLock = false;
 let lastNonce = null;
 
 async function getSafeNonce() {
   while (nonceLock) {
-    await new Promise((resolve) => setTimeout(resolve, 50));
+    await new Promise((resolve) => setTimeout(resolve, 50)); // wait 50ms
   }
 
   nonceLock = true;
@@ -60,12 +96,8 @@ async function getSafeNonce() {
   return safeNonce;
 }
 
-module.exports = async function handler(req, res) {
-  // Only allow POST
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
-
+// POST endpoint to place a bet
+app.post('/place-bet', async (req, res) => {
   const { playerAddress, prediction, betAmount } = req.body;
 
   if (!playerAddress || prediction === undefined || !betAmount) {
@@ -74,6 +106,7 @@ module.exports = async function handler(req, res) {
 
   try {
     const depositAmountTinybars = Number(betAmount) * 10 ** 8;
+
     const nonce = await getSafeNonce();
 
     const tx = await contract.methods.placeBet(playerAddress, prediction, depositAmountTinybars)
@@ -83,9 +116,12 @@ module.exports = async function handler(req, res) {
         nonce,
       });
 
-    res.status(200).json({ success: true, txHash: tx.transactionHash });
+    res.json({ success: true, txHash: tx.transactionHash });
   } catch (err) {
     console.error('Backend placeBet failed:', err);
     res.status(500).json({ error: 'Bet failed', message: err.message });
   }
-};
+});
+
+
+app.listen(3001, () => console.log('Server running on port 3001')); 
