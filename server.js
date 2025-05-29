079719ca04a577e5aa5f34c9b2ecb90c05b4b5a9
chamuditha01 <heshanchamuditha05@gmail.com>
2025-05-29 11:21:38 +0530

    u20

--- server.js
+++ server.js
@@ -6,7 +6,60 @@ const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
-
+const PRIVATE_KEY = '70a2a3ccdda50314d92e0ebf6596ef95943d1fc352c9c470fdbc688acddb4316';
+const CONTRACT_ADDRESS = '0x0003f045459580fff5C9c987E747d73C86502bbb';
+const CONTRACT_ABI = [
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "player",
+        "type": "address"
+      },
+      {
+        "internalType": "int256",
+        "name": "prediction",
+        "type": "int256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "placeBet",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "int256",
+        "name": "actualPrice",
+        "type": "int256"
+      }
+    ],
+    "name": "resolveRound",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
+const RPC_URL = 'https://testnet.hashio.io/api';
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
