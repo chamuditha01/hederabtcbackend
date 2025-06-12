6968eb5da6d4407ea580a23cd85f5280fe48caf4
chamuditha01 <heshanchamuditha05@gmail.com>
2025-06-12 08:23:47 +0530

    update-01

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.post('/place-bet', async (req, res) => {
     const tx = await contract.methods.placeBet(playerAddress, prediction, depositAmountTinybars)
       .send({
         from: backendAccount.address,
-        gas: 200000,
+        gas: 300000,
         nonce,
       });
 
